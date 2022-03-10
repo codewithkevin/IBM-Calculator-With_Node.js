be13ab789aaad4df8dd6af28a5bfc64ae022adf8
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-10 14:31:36 +0000

    update

--- ibm.js
+++ ibm.js
@@ -3,5 +3,11 @@ const express = require("express")
 const app = express()
 
 app.get("/", function (req, res){
-    res.sendFile
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+
+app.listen(4000, function(){
+    console.log("Server is running on port 4000")
 })
