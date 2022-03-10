556b8aa1f3bd67d324b87ef6b7b8df244d32135a
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-10 14:37:17 +0000

    update

--- ibm.js
+++ ibm.js
@@ -1,11 +1,25 @@
 const express = require("express")
 
+const bodypasser = require("body-passer")
+
 const app = express()
+
+app.use(bodypasser.urlencoded({extended: true}));
 
 app.get("/", function (req, res){
     res.sendFile(__dirname + "/index.html")
 })
 
+
+app.post("/", function(req, res){
+    var num1 = req.body.num1;
+    
+    var num2 = req.body.num2;
+
+    var result = num1 + num2;
+
+    res.send("Your result is: " + result)
+})
 
 
 app.listen(4000, function(){
