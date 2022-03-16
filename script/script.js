80e764d074bee4bd20c09b9f591b2a22f514a207
Kevin Black <86974174+codewithkevin@users.noreply.github.com>
2022-03-16 12:40:52 +0000

    update

--- script/script.js
+++ script/script.js
@@ -4,5 +4,12 @@ function BMI() {
     var bmi=w/(h/100*h/100);
     var bmio=(bmi.toFixed(2));
 
+
+    if(bmio < 45){
+        document.getElementById("result").innerHTML="Your BMI is " + bmio + " you are under weight";
+    }
+
+    else if(bmio > 45  )
+
     document.getElementById("result").innerHTML="Your BMI is " + bmio;
 }
